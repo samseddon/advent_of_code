e86ee16067ba52168705717b813092eaa20d2d0e
samseddon <samuel.seddon@gmail.com>
2023-12-07 19:22:56 +0100

    up to date

--- 2015/day_5/day_5-2.py
+++ 2015/day_5/day_5-2.py
@@ -2,14 +2,13 @@
 
 def rule1(line):
     c = 0 
-    for char in line:
-        print(char)
-        if line.count(char)>1:
-            pos = [pos for pos, c in enumerate(line) if c == char]
-            print(pos)
-            for _ in range(1, len(pos)):
-                if pos[_] - pos[_-1] > 1:
-                    c += 1
+    for _ in range(0, len(line)-1, 1):
+        double = line[_] + line[_+1]
+        if len(line)-len(line.replace(double, "")) > 2:
+            c += 1
+            
+
+
                 
     if c == 0:
         return False
@@ -19,13 +18,12 @@ def rule2(line):
     for _ in range(0, len(line)-2, 1):
         if line[_] == line[_+2]:
             c += 1
-            print("YES")
     if c == 0:
         return False
 
 if __name__ == "__main__":
     f = open("input.txt")
-    f = open("test-input.txt")
+    #f = open("test-input.txt")
     Lines = f.readlines()
     nice = []
     for line in Lines:
